e931d31b3a1ead270bfb519f486c2d7d4d61cff5
Saksham Sharma <84015186+SakshamSharma09@users.noreply.github.com>
2022-07-12 22:30:22 +0530

    Time: 5 ms (6.33%), Space: 54.3 MB (39.41%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length<3){
+            return false;
+        }
+        int high=-1;
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
+                high=i;
+            }
+        }
+        System.out.println(high);
+        if(high==-1){
+            return false;
+        }
+        for(int i=0;i<high;i++){
+            if(arr[i] >= arr[i+1]){
+                return false;
+            }
+        }
+        for(int j=high;j<arr.length-1;j++){
+            if(arr[j] <= arr[j+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
