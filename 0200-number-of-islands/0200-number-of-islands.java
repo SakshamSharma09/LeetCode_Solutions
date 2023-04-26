7106812e6c4e0a61bc748f1c8260f21a5e548381
Saksham Sharma <sakshamjoshi09122002@gmail.com>
2023-04-26 13:16:58 +0530

    Time: 3 ms (93.88%), Space: 50.9 MB (74.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,12 +1,15 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        int totalIslands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j]=='1' && !visited[i][j]){
-                    island(grid, i, j, visited);
-                    totalIslands++;
+        int m = grid.length; // total rows
+        int n = grid[0].length; // total columns
+        boolean[][] visited = new boolean[m][n]; //array to check if the element is visited or not 
+        int totalIslands = 0; //island counter
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j]=='1' && !visited[i][j]){  // if there is land and not visited call its neighbours
+                    island(grid, i, j, visited); // DFS approachs to call neighbours
+                    totalIslands++;  // after getting 1 island increment
                 }
             }
         }
@@ -14,8 +17,9 @@ class Solution {
     }
 
     public void island(char[][] grid,int i, int j, boolean[][] visited){
-        visited[i][j] = true;
-            
+        visited[i][j] = true;  // not visited now make visited 
+        
+            // check upwards
         if(i>0 && grid[i-1][j]=='1'&& !visited[i-1][j]){
             island(grid,i-1, j, visited);
         }
