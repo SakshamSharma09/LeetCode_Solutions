2da8d39ecf17dd7f65d63bd8bb869076c8c1b73f
Saksham Sharma <sakshamjoshi09122002@gmail.com>
2023-05-03 11:15:18 +0530

    Time: 11 ms (89.71%), Space: 55.9 MB (78.36%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<nums.length;i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+            }
+            else{
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+}
